01cbf439ff0c95af79cfce2bb9d04dfe71c23333
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-07 20:22:04 +0000

    Add files via upload

---
+++ cap8-exercicios-teoricos.py
@@ -0,0 +1,26 @@
+'''
+Exercícios Teoricos | Capítulo 8
+
+1) O que entende por imagem bipmap?
+
+2) O que entende por pixel?
+
+3) O que entende por resolução de uma imagem?
+
+4) O que entende por modo RGB?
+
+5) A ordem e o modo de percorrer uma imagem são irrelevantes. Concorda com esta afirmação?
+
+6) Quais as vantagens e os inconvenientes de usar o mecanismo de abstração procedimental?
+
+7) O que entende por filtro ou máscara?
+
+8) O que entende por convolução?
+
+9) O que entende por operadores de Sobel?
+
+10) Quanto maior for o filtro usado, melhor se consegue o efeito pretendido. Concorda com esta afirmação?
+
+11) Quando se usa um filtro, por que razão se cria uma nova imagem?
+
+'''
